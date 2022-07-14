f43daebefdd2046c661880d141b2a3e5f5af1f92
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-14 12:46:23 -0300

    get purchase orders by id

--- app.py
+++ app.py
@@ -33,4 +33,12 @@ def get_purchase_orders():
     return jsonify(purchase_orders)
 
 
+@app.route('/purchase_orders/<int:id>')
+def get_purchase_orders_by_id(id):
+    for po in purchase_orders:
+        if po['id'] == id:
+            return jsonify(po)
+    return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
+
+
 app.run(port=5000)
