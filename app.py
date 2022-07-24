94f8b79529cb5cc1db1b4652ebc1fc6f47ae9bbe
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-24 13:27:08 -0300

    post purchase_orders e get_purchase_orders_by_id

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from flask_restful import Api
 
-from purchase_orders.resources import PurchaseOrders
+from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
 
 app = Flask(__name__)
 api = Api(app)
 
 api.add_resource(PurchaseOrders, '/purchase_orders')
+api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
 
 app.run(port=5000)
--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 purchase_orders = [
     {
@@ -17,5 +17,43 @@ purchase_orders = [
 
 
 class PurchaseOrders(Resource):
+
+    parser = reqparse.RequestParser()
+
+    parser.add_argument(
+        'id',
+        type=int,
+        required=True,
+        help="Informe um id"
+    )
+
+    parser.add_argument(
+        'description',
+        type=str,
+        required=True,
+        help="Informe uma descrição"
+    )
+
     def get(self):
         return jsonify(purchase_orders)
+
+    def post(self):
+        request_data = PurchaseOrders().parser.parse_args()
+
+        purchase_order = {
+            'id': request_data['id'],
+            'description': request_data['description'],
+            'items': []
+        }
+
+        purchase_orders.append(purchase_order)
+
+        return jsonify(purchase_order)
+
+
+class PurchaseOrdersById(Resource):
+    def get(self, id):
+        for po in purchase_orders:
+            if po['id'] == id:
+                return jsonify(po)
+        return jsonify({'message': 'Pedido {} não encontrado'.format(id)})
