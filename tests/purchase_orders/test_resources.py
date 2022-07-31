9978e05796b26cb3e05fce00b1f96f954c4d76e4
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-31 16:16:24 -0300

    testando cenários de falha post puchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,5 +1,6 @@
 import json
 
+
 def test_get_purchase_orders(test_client):
     response = test_client.get('/purchase_orders')
 
@@ -8,6 +9,7 @@ def test_get_purchase_orders(test_client):
     assert response.json[0]['description'] == 'Purchase ORder id 1'
     assert len(response.json[0]['items']) == 1
     assert response.json[0]['items'][0]['id'] == 1
+
 
 def test_post_purchase_orders(test_client):
     obj = {'id': 2, 'description': 'Purchase Order id 2'}
@@ -22,3 +24,25 @@ def test_post_purchase_orders(test_client):
     assert response.json['id'] == obj['id']
     assert response.json['description'] == obj['description']
     assert response.json['items'] == []
+
+
+def test_post_empty_id(test_client):
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps({'description': 'Descrição'}),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+    assert response.json['message']['id'] == 'Informe um ID válido'
+
+
+def test_post_empty_description(test_client):
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps({'id': 2}),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 400
+    assert response.json['message']['description'] == 'Informe uma descrição válida'
