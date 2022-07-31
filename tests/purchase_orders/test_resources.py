28b62fd1aef39870b9ffbaf0d17d81f14c4dd7ee
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-31 16:00:46 -0300

    testes post purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,3 +1,5 @@
+import json
+
 def test_get_purchase_orders(test_client):
     response = test_client.get('/purchase_orders')
 
@@ -6,3 +8,17 @@ def test_get_purchase_orders(test_client):
     assert response.json[0]['description'] == 'Purchase ORder id 1'
     assert len(response.json[0]['items']) == 1
     assert response.json[0]['items'][0]['id'] == 1
+
+def test_post_purchase_orders(test_client):
+    obj = {'id': 2, 'description': 'Purchase Order id 2'}
+
+    response = test_client.post(
+        '/purchase_orders',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == obj['id']
+    assert response.json['description'] == obj['description']
+    assert response.json['items'] == []
