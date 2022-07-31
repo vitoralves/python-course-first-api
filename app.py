86b378d2536e2e56aaefe9cd270e57292efa3315
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-31 14:54:55 -0300

    Refatoração para utilizar TDD

--- app.py
+++ app.py
@@ -1,14 +1,11 @@
 from flask import Flask
 from flask_restful import Api
 
-from purchase_orders.resources import PurchaseOrders, PurchaseOrdersById
-from purchase_orders_items.resources import PurchaseOrdersItems
 
-app = Flask(__name__)
-api = Api(app)
+def create_app():
+    app = Flask(__name__)
+    api = Api(app)
 
-api.add_resource(PurchaseOrders, '/purchase_orders')
-api.add_resource(PurchaseOrdersById, '/purchase_orders/<int:id>')
-api.add_resource(PurchaseOrdersItems, '/purchase_orders/<int:id>/items')
+    app.run(port=5000)
 
-app.run(port=5000)
+    return app
