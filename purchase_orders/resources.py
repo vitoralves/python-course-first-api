a81fea1d2727b96405be09338dcc85d975d0ff98
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-07-31 16:09:19 -0300

    refatorando para inserir o post purchase_orders

--- purchase_orders/resources.py
+++ purchase_orders/resources.py
@@ -1,5 +1,5 @@
 from flask import jsonify
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 purchase_orders = [
     {
@@ -17,6 +17,32 @@ purchase_orders = [
 
 
 class PurchaseOrders(Resource):
+    parser = reqparse.RequestParser()
+    parser.add_argument(
+        'id',
+        type=int,
+        required=True,
+        help='Informe um ID válido'
+    )
+    parser.add_argument(
+        'description',
+        type=str,
+        required=True,
+        help='Informe uma descrição válida'
+    )
 
     def get(self):
         return jsonify(purchase_orders)
+
+    def post(self):
+        data = PurchaseOrders.parser.parse_args()
+
+        purchase_order = {
+            'id': data['id'],
+            'description': data['description'],
+            'items': []
+        }
+
+        purchase_orders.append(purchase_order)
+
+        return jsonify(purchase_order)
