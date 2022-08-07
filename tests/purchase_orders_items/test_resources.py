a11837080ddfb7aaf7c1e1ab760d9bf75c160d06
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-08-07 14:23:47 -0300

    test post purchase order item

--- tests/purchase_orders_items/test_resources.py
+++ tests/purchase_orders_items/test_resources.py
@@ -1,3 +1,6 @@
+import json
+
+
 def test_get_items_by_purchase_order_id(test_client):
     response = test_client.get('/purchase_orders/1/items')
 
@@ -15,3 +18,24 @@ def test_get_items_by_purchase_order_id_not_found(test_client):
     assert response.status_code == 200
     assert response.json['message'] == 'Pedido de id {} não encontrado'.format(
         id)
+
+
+def test_post_purchase_order_item(test_client):
+    obj = {
+        'id': 2,
+        'description': 'Item teste',
+        'price': 10.0
+    }
+
+    response = test_client.post(
+        '/purchase_order_items/1/items',
+        data=json.dumps(obj),
+        content_type='application/json'
+    )
+
+    assert response.status_code == 200
+    assert response.json['id'] == 1
+    assert len(response.json['items']) == 2
+    assert response.json['items'][1]['id'] == obj['id']
+    assert response.json['items'][1]['description'] == obj['description']
+    assert response.json['items'][1]['price'] == obj['price']
