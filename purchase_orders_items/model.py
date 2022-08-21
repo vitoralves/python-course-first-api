6100d0b7413b813d24823ce5ca218fc6126c7ae3
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-08-21 14:29:45 -0300

    purchase_order_items_model

---
+++ purchase_orders_items/model.py
@@ -0,0 +1,27 @@
+from db import db
+
+
+class PurchaseOrderItemsModel(db.Model):
+    __tablename__ = 'purchase_orders_items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(500), nullable=False)
+    price = db.Column(db.Float(precision=2), nullable=False)
+    purchase_order_id = db.Column(db.Integer, db.ForeignKey(
+        'purchase_order.id'), nullable=False)
+
+    def __init__(self, description, price, purchase_order_id):
+        self.description = description
+        self.price = price
+        self.purchase_order_id = purchase_order_id
+
+    def as_dict(self):
+        return {c.name: getattr(c.name) for c in self.__table__.columns}
+
+    @classmethod
+    def find_by_purchase_order_id(cls, _purchase_order_id):
+        return cls.query.filter_by(_purchase_order_id=_purchase_order_id).all()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
