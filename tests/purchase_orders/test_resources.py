91511aa298d99d034e6f52aaf347356dd15f45e3
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-08-28 17:02:07 -0300

    refatorando os demais testes para purchase_orders

--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -10,7 +10,7 @@ def test_get_purchase_orders(test_client, seed_db):
 
 
 def test_post_purchase_orders(test_client):
-    obj = {'id': 2, 'description': 'Purchase Order id 2'}
+    obj = {'description': 'Purchase Order id 2'}
 
     response = test_client.post(
         '/purchase_orders',
@@ -19,26 +19,14 @@ def test_post_purchase_orders(test_client):
     )
 
     assert response.status_code == 200
-    assert response.json['id'] == obj['id']
+    assert response.json['id'] is not None
     assert response.json['description'] == obj['description']
-    assert response.json['items'] == []
-
-
-def test_post_empty_id(test_client):
-    response = test_client.post(
-        '/purchase_orders',
-        data=json.dumps({'description': 'Descrição'}),
-        content_type='application/json'
-    )
-
-    assert response.status_code == 400
-    assert response.json['message']['id'] == 'Informe um ID válido'
 
 
 def test_post_empty_description(test_client):
     response = test_client.post(
         '/purchase_orders',
-        data=json.dumps({'id': 2}),
+        data=json.dumps({}),
         content_type='application/json'
     )
 
@@ -46,12 +34,12 @@ def test_post_empty_description(test_client):
     assert response.json['message']['description'] == 'Informe uma descrição válida'
 
 
-def test_get_purchase_order_by_id(test_client):
-    response = test_client.get('/purchase_orders/1')
+def test_get_purchase_order_by_id(test_client, seed_db):
+    response = test_client.get('/purchase_orders/{}'.format(seed_db.id))
 
     assert response.status_code == 200
-    assert response.json['id'] == 1
-    assert response.json['description'] == 'Purchase ORder id 1'
+    assert response.json['id'] == seed_db.id
+    assert response.json['description'] == seed_db.description
 
 
 def test_get_purchase_order_not_found(test_client):
