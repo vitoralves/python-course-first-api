e4380d92838578d171bdb57e7ba56dc60ac22473
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-08-28 16:52:09 -0300

    conftest para test_purchase_orders

---
+++ tests/purchase_orders/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+from db import db
+
+from purchase_orders.model import PurchaseOrderModel
+
+
+@pytest.fixture()
+def seed_db():
+    po = PurchaseOrderModel('Purchase Order Teste')
+    db.session.add(po)
+    db.session.commit()
+
+    yield po
+
+
+@pytest.fixture(scope='function', autouse=True)
+def clear_db():
+    db.session.query(PurchaseOrderModel).delete()
+    db.session.commit()
--- tests/purchase_orders/test_resources.py
+++ tests/purchase_orders/test_resources.py
@@ -1,14 +1,12 @@
 import json
 
 
-def test_get_purchase_orders(test_client):
+def test_get_purchase_orders(test_client, seed_db):
     response = test_client.get('/purchase_orders')
 
     assert response.status_code == 200
-    assert response.json[0]['id'] == 1
-    assert response.json[0]['description'] == 'Purchase ORder id 1'
-    assert len(response.json[0]['items']) == 1
-    assert response.json[0]['items'][0]['id'] == 1
+    assert response.json[0]['id'] == seed_db.id
+    assert response.json[0]['description'] == seed_db.description
 
 
 def test_post_purchase_orders(test_client):
