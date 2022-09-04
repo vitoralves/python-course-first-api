d850f792ef7f2a95eba362e113c8f779d48b279f
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-09-04 16:52:09 -0300

    validação _checkquantity

--- purchase_orders/services.py
+++ purchase_orders/services.py
@@ -1,14 +1,21 @@
 from flask import jsonify
 from .model import PurchaseOrderModel
+from .exceptions import QuantityException
 
 
 class PurchaseOrdersService:
+
+    def _check_quantity(self, quantity):
+        if quantity < 50 or quantity > 150:
+            raise QuantityException(
+                'A quantidade deve ser entre 50 e 150 itens')
 
     def find_all(self):
         purchase_orders = PurchaseOrderModel.find_all()
         return [p.as_dict() for p in purchase_orders]
 
     def create(self, **kwargs):
+        self._check_quantity(kwargs['quantity'])
         purchase_order = PurchaseOrderModel(**kwargs)
         purchase_order.save()
 
