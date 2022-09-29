fc81189ed48195e4d3c9d5c61115819810ce183c
Vitor Alves <vitor.diogo.live@hotmail.com>
2022-09-29 12:36:24 -0300

    modelo para usuários

---
+++ users/model.py
@@ -0,0 +1,28 @@
+from db import db
+from passlib.hash import pbkdf2_sha256
+
+
+class UsersModel(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary=True)
+    email = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(300), nullable=False)
+
+    def __init__(self, email, password):
+        email = email
+        password = pbkdf2_sha256.hash(password)
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "email": self.email
+        }
+
+    @classmethod
+    def find_user_by_email(cls, _email):
+        return cls.query.filter_by(email=_email).first()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
